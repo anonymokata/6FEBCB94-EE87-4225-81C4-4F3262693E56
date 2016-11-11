2a121ee1ab613e1349f508cbb05f911bf53903b6
anonymous <anonymous>
2016-11-11 10:16:54 -0500

    Tests pass with actual logic

--- rpn_lib.c
+++ rpn_lib.c
@@ -1,13 +1,66 @@
 #include <string.h>
 #include "rpn.h"
+typedef int bool;
+
+#define RAII(type, name, size) \
+    type name[size];\
+    memset(name, 0, size);
+
+#define PUSH(var, index, value) \
+    var[index++] = value;
+
+#define POP_FROM_START(var, index) \
+    var[index++]
+
+#define true 1
+#define false 0
+#define null NULL
+
+bool valid_operand(char c)
+{
+    if (c >= 'a' && c <= 'z')
+        return true;
+    return false;
+}
+
+bool valid_operator(char c)
+{
+    return c == '^' || c == '/' || c == '*' || c == '-' || c == '+';
+}
 
 char* infix_to_rpn(const char* infix, int infixmaxlen, char* buff, int bufflen)
 {
-    if (buff == NULL || bufflen <= 0)
+    if (buff == null || bufflen <= 0)
     {
-        return NULL;
+        return null;
     }
-    if (strcmp(infix, "a+b") == 0)
-        memcpy(buff, "ab+", 3);
+    RAII(char, operands, 1 * 1024);
+    RAII(char, operators, 1 * 1024);
+    int opdIndexMax = 0, oprIndexMax = 0;
+    for(int i = 0; i < infixmaxlen && infix[i] != '\0'; i++)
+    {
+        char inf = infix[i];
+        if (valid_operand(inf))
+        {
+            PUSH(operands, opdIndexMax, inf);
+        }
+        else if (valid_operator(inf))
+        {
+            PUSH(operators, oprIndexMax, inf);
+        }
+        else
+            return null;
+    }
+    int buffIndex = 0;
+    int opdIndex = 0;
+    do
+    {
+        PUSH(buff, buffIndex, POP_FROM_START(operands, opdIndex));
+    } while(opdIndex < opdIndexMax);
+    int oprIndex = 0;
+    do
+    {
+        PUSH(buff, buffIndex, POP_FROM_START(operators, oprIndex));
+    } while(oprIndex < oprIndexMax);
     return buff;
 }
