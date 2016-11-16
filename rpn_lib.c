41b0cf8ce20846c18e92fb77156fd7bdea0f8443
anonymous <anonymous>
2016-11-16 13:37:50 -0500

    Refactor to recursive, more tests with parens, buffer checking, allow no malloc

--- rpn_lib.c
+++ rpn_lib.c
@@ -1,14 +1,28 @@
+#include <stdlib.h>
 #include <string.h>
 #include "rpn.h"
 typedef int bool;
 
+#define MEMSET(buff, size) \
+    memset(buff, 0, size);
+
 #define RAII(type, name, size) \
     type name[size];           \
-    memset(name, 0, size);
+    MEMSET(name, size);
+
+#define DRAII(type, name, size)               \
+    type *name = malloc(sizeof(type) * size); \
+    MEMSET(name, size);
+
+#define DRAII_EXISTING(type, name, size) \
+    name = malloc(sizeof(type) * size);  \
+    MEMSET(name, size);
 
 #define true 1
 #define false 0
+#ifndef null
 #define null NULL
+#endif
 #define byte unsigned char
 
 char operator_lookup[] = {'^', '/', '*', '-', '+'};
@@ -47,66 +61,146 @@ byte find_op_value(char op)
 
 bool op_less_than(char op0, char op1)
 {
+    if (op1 == '(')
+        return true;
     byte op0val = find_op_value(op0);
     byte op1val = find_op_value(op1);
     return op0val - op1val < 0;
 }
 
-int opindex = 0;
-char opbuff[128];//TODO: remove global
-void push_op(char op)
+char peek(char *buff, int *index)
 {
-    if (opindex > 0 && opbuff[opindex - 1] != '(' && !op_less_than(op, opbuff[opindex - 1]))
+    return buff[*index - 1];
+}
+
+void push(char *buff, int *index, char c)
+{
+    buff[(*index)++] = c;
+}
+
+char pop(char *buff, int *index)
+{
+    if (*index > 0)
+        return buff[--(*index)];
+    return 0;
+}
+
+void push_op(char *buff, int *index, char op)
+{
+    if (*index > 0 && buff[*index - 1] != '(' && !op_less_than(op, buff[*index - 1]))
     {
-        opbuff[opindex] = opbuff[opindex - 1];
-        opbuff[opindex - 1] = op;
-        opindex++;
+        buff[*index] = buff[*index - 1];
+        buff[*index - 1] = op;
+        (*index)++;
     }
     else
     {
-        opbuff[opindex++] = op;
+        push(buff, index, op);
     }
 }
 
-void infix_to_rpn_impl(const char *infix, int *index, char *buff, int *buffindex)
+void swap(char *a, char *b)
 {
-    char inf = infix[(*index)++];
+    char c = *a;
+    *a = *b;
+    *b = c;
+}
+
+bool infix_to_rpn_impl(const char *infix, int infixmaxlen, int index, char *buff, int *buffindex, char *scratchbuff, int *scratchbufflen)
+{
+    if (infix[index] == '\0' || index >= infixmaxlen)
+    {
+        if (*scratchbufflen > 0)
+        {
+            char op;
+            do
+            {
+                op = pop(scratchbuff, scratchbufflen);
+                if (op != '(')
+                {
+                    push(buff, buffindex, op);
+                }
+            } while (op != '(' && *scratchbufflen > 0);
+        }
+        return true;
+    }
+    char inf = infix[index];
     if (valid_operand(inf))
     {
-        buff[(*buffindex)++] = inf;
+        push(buff, buffindex, inf);
+        bool ret = infix_to_rpn_impl(infix, infixmaxlen, index + 1, buff, buffindex, scratchbuff, scratchbufflen);
+        if (!ret)
+            return ret;
     }
     else if (valid_operator(inf))
     {
-        push_op(inf);
+        if (*scratchbufflen == 0 || (*scratchbufflen > 0 && op_less_than(inf, peek(scratchbuff, scratchbufflen))))
+        {
+            push(scratchbuff, scratchbufflen, inf);
+        }
+        else if (*scratchbufflen > 0 && !op_less_than(inf, peek(scratchbuff, scratchbufflen)))
+        {
+            push(scratchbuff, scratchbufflen, inf);
+            while (*scratchbufflen > 1 && scratchbuff[*scratchbufflen - 2] != '(' && op_less_than(scratchbuff[*scratchbufflen - 2], scratchbuff[*scratchbufflen - 1]))
+            {
+                swap(&scratchbuff[*scratchbufflen - 1], &scratchbuff[*scratchbufflen - 2]);
+                push(buff, buffindex, pop(scratchbuff, scratchbufflen));
+            }
+        }
+        else
+        {
+            push(scratchbuff, scratchbufflen, inf);
+        }
+        bool ret = infix_to_rpn_impl(infix, infixmaxlen, index + 1, buff, buffindex, scratchbuff, scratchbufflen);
+        if (!ret)
+            return ret;
     }
     else if (valid_open_paren(inf))
     {
-        opbuff[opindex++] = '(';
+        push_op(scratchbuff, scratchbufflen, inf);
+        bool ret = infix_to_rpn_impl(infix, infixmaxlen, index + 1, buff, buffindex, scratchbuff, scratchbufflen);
+        if (!ret)
+            return ret;
     }
     else if (valid_close_paren(inf))
     {
+        char op;
         do
         {
-            if (opbuff[opindex - 1] == '(')
-                break;
-            buff[(*buffindex)++] = opbuff[opindex - 1];
-        } while (opbuff[opindex--]);
+            op = pop(scratchbuff, scratchbufflen);
+            if (op != '(')
+            {
+                push(buff, buffindex, op);
+            }
+        } while (op != '(' && *scratchbufflen > 0);
+        bool ret = infix_to_rpn_impl(infix, infixmaxlen, index + 1, buff, buffindex, scratchbuff, scratchbufflen);
+        if (!ret)
+            return ret;
     }
+    return true;
 }
 
-char *infix_to_rpn(const char *infix, int infixmaxlen, char *buff, int bufflen)
+char *infix_to_rpn(const char *infix, int infixmaxlen, char *buff, int bufflen, char *scratchbuff, int scratchbufflen)
 {
-    memset(opbuff, 0, sizeof(opbuff));
-    int iindex = 0, buffindex = 0;
-    while (infix[iindex] && iindex < infixmaxlen)
+    if (infix == null || buff == null)
+        return null;
+    MEMSET(buff, bufflen);
+    bool dynamic = scratchbuff == null || scratchbufflen == 0;
+    if (dynamic)
     {
-        infix_to_rpn_impl(infix, &iindex, buff, &buffindex);
+        scratchbufflen = infixmaxlen;
+        DRAII_EXISTING(char, scratchbuff, scratchbufflen);
     }
-    while (opindex > 0)
+    else
     {
-        if (opbuff[opindex - 1] != '(')
-            buff[buffindex++] = opbuff[opindex - 1];
-        opindex--;
+        MEMSET(scratchbuff, scratchbufflen);
     }
-    return buff;
+    int sbl = 0;
+    int bl = 0;
+    bool ret = infix_to_rpn_impl(infix, infixmaxlen, 0, buff, &bl, scratchbuff, &sbl);
+    if (dynamic)
+    {
+        free(scratchbuff);
+    }
+    return ret ? buff : null;
 }
--- tests.c
+++ tests.c
@@ -7,59 +7,76 @@
 
 START_TEST(infix_op_check)
 {
-    ck_assert_ptr_eq(infix_to_rpn("a@b", 0, NULL, 0), NULL);
+    ck_assert_ptr_eq(infix_to_rpn("a@b", 0, NULL, 0, NULL, 0), NULL);
 }
 END_TEST
 
 START_TEST(infix_max_len_check)
 {
-    ck_assert_ptr_eq(infix_to_rpn("a+b", 0, NULL, 0), NULL);
+    ck_assert_ptr_eq(infix_to_rpn("a+b", 0, NULL, 0, NULL, 0), NULL);
 }
 END_TEST
 
 START_TEST(buff_len_check)
 {
-    ck_assert_ptr_eq(infix_to_rpn("a+b", 0, NULL, 0), NULL);
+    ck_assert_ptr_eq(infix_to_rpn("a+b", 0, NULL, 0, NULL, 0), NULL);
 }
 END_TEST
+char buff[128], scratchbuff[128];
 
 START_TEST(a_plus_b)
 {
-    char buff[128];
     memset(buff, 0, sizeof(buff));
-    ck_assert_str_eq(infix_to_rpn("a+b", 3, buff, sizeof(buff)), "ab+");
+    ck_assert_str_eq(infix_to_rpn("a+b", 3, buff, sizeof(buff), scratchbuff, sizeof(scratchbuff)), "ab+");
 }
 END_TEST
 
 START_TEST(a_plus_b_minus_c)
 {
-    char buff[128];
     memset(buff, 0, sizeof(buff));
-    ck_assert_str_eq(infix_to_rpn("a+b-c", 5, buff, sizeof(buff)), "abc-+");
+    ck_assert_str_eq(infix_to_rpn("a+b-c", 5, buff, sizeof(buff), scratchbuff, sizeof(scratchbuff)), "abc-+");
 }
 END_TEST
 
 START_TEST(a_plus_p_b_minus_c_p)
 {
-    char buff[128];
     memset(buff, 0, sizeof(buff));
-    ck_assert_str_eq(infix_to_rpn("a+(b-c)", 7, buff, sizeof(buff)), "abc-+");
+    ck_assert_str_eq(infix_to_rpn("a+(b-c)", 7, buff, sizeof(buff), scratchbuff, sizeof(scratchbuff)), "abc-+");
 }
 END_TEST
 
 START_TEST(a_plus_b_minus_c_minus_d)
 {
-    char buff[128];
     memset(buff, 0, sizeof(buff));
-    ck_assert_str_eq(infix_to_rpn("a+b-c-d", 7, buff, sizeof(buff)), "abcd--+");
+    ck_assert_str_eq(infix_to_rpn("a+b-c-d", 7, buff, sizeof(buff), scratchbuff, sizeof(scratchbuff)), "abcd--+");
 }
 END_TEST
 
 START_TEST(p_a_plus_b_p_minus_c)
 {
-    char buff[128];
     memset(buff, 0, sizeof(buff));
-    ck_assert_str_eq(infix_to_rpn("(a+b)-c", 7, buff, sizeof(buff)), "ab+c-");
+    ck_assert_str_eq(infix_to_rpn("(a+b)-c", 7, buff, sizeof(buff), scratchbuff, sizeof(scratchbuff)), "ab+c-");
+}
+END_TEST
+
+START_TEST(abcdef)
+{
+    memset(buff, 0, sizeof(buff));
+    ck_assert_str_eq(infix_to_rpn("a+b-c*d/e^f", sizeof(buff), buff, sizeof(buff), scratchbuff, sizeof(scratchbuff)), "abcdef^/*-+");
+}
+END_TEST
+
+START_TEST(abcdef_short)
+{
+    memset(buff, 0, sizeof(buff));
+    ck_assert_str_eq(infix_to_rpn("a+b-c*d/e^f", 3, buff, sizeof(buff), scratchbuff, sizeof(scratchbuff)), "ab+");
+}
+END_TEST
+
+START_TEST(l_div_m_pow_n_mult_o_minus_p)
+{
+    memset(buff, 0, sizeof(buff));
+    ck_assert_str_eq(infix_to_rpn("l/m^n*o-p", 9, buff, sizeof(buff), scratchbuff, sizeof(scratchbuff)), "lmn^/o*p-");
 }
 END_TEST
 
@@ -76,6 +93,9 @@ Suite *rpn_suite()
     tcase_add_test(tc_core, a_plus_p_b_minus_c_p);
     tcase_add_test(tc_core, a_plus_b_minus_c_minus_d);
     tcase_add_test(tc_core, p_a_plus_b_p_minus_c);
+    tcase_add_test(tc_core, abcdef);
+    tcase_add_test(tc_core, abcdef_short);
+    tcase_add_test(tc_core, l_div_m_pow_n_mult_o_minus_p);
     suite_add_tcase(s, tc_core);
     return s;
 }
